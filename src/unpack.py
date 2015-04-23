e706b868ca3b6b27ce99797af2596c7f4d8d668b
Chunis Deng <chunchengfh@gmail.com>
2015-04-23 23:09:02 +0800

    add unpack.py, prepare for un-pack archives

---
+++ src/unpack.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import subprocess
+import os, time
+import shutil
+
+UNPACK_LOG = "unpack.log"
+TMP_DIR = "bookmate_tmp"
+
+def unpack_file(zipfile, outpath='.', tool='patool'):
+	tmpdir = os.path.join(outpath, TMP_DIR)
+	try:
+		if not os.path.exists(outpath):
+			os.mkdir(outpath)
+		os.mkdir(tmpdir)
+	except OSError, e:
+		print "mkdir %s failed: %s" %(tmpdir, str(e))
+		return -1
+
+	try:
+		cmd = [tool, "extract", "--outdir", tmpdir, zipfile]
+		#print 'cmd:', cmd
+		subprocess.check_output(cmd)
+	except OSError:
+		print 'Got OSERROR'
+	except subprocess.CalledProcessError as e:
+		print "unpack_file() failed!"
+		fl = open(UNPACK_LOG, 'a')
+		fl.write('\n------' + time.asctime() + '------\n')
+		for c in cmd:
+			fl.write("%s " %c)
+		fl.write('\n')
+		fl.write(e.output)
+		fl.write("\n")
+		fl.flush()
+		fl.close()
+		return -1
+	else:
+		all_files = os.listdir(tmpdir)
+		if len(all_files) == 1:
+			shutil.move(os.path.join(tmpdir, all_files[0]), outpath)
+			os.rmdir(tmpdir)
+		elif len(all_files) > 1:
+			# TODO: strip sufix such as 'tar.gz' or 'rar'
+			newdir = os.path.join(outpath, os.path.basename(zipfile))
+			os.rename(tmpdir, newdir)
+	return 0
+
+
+# Let's test it:
+#unpack_file("test.gz", 'mydir')
+unpack_file("test.tar.gz", 'mydir')
+#unpack_file("test.rar", 'mydir')
+#unpack_file("test.7z", 'mydir')
+
+
