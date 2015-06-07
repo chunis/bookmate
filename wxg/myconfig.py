0c4f09961df00396598a678851a750af07495636
Chunis Deng <chunchengfh@gmail.com>
2015-06-07 23:34:02 +0800

    add outer layer info pages for generic/extract/duplicate UI

--- wxg/myconfig.py
+++ wxg/myconfig.py
@@ -13,11 +13,47 @@ from rename import ReName
 from same_name import SameName
 
 
-config_config = [("ignore"+ ' '*30, ConfigIgnore),  ("Setting path", ConfigPath)]
-config_dupli = [("Keep a single copy", DuplicateKeep),  ("remove archive files", DuplicateRemove)]
-config_extract = [("extract to", ExtractTo),  ("extract succeed", ExtractOK)]
-config_rename = [("rename", ReName)]
-config_same_name = [("same name", SameName)]
+generic_str = "Configurations for searched directories, ignored directories and file types"
+extract_str = "Configurations for Extract"
+duplicate_str = "Configurations for Search and remove Duplicated files"
+
+
+class GenericInfo(wx.Panel):
+    def __init__(self, *args, **kwds):
+        wx.Panel.__init__(self, *args, **kwds)
+        self.label_hidden_notes = wx.StaticText(self, wx.ID_ANY, _(generic_str))
+
+        sizer_4 = wx.BoxSizer(wx.VERTICAL)
+        sizer_4.Add(self.label_hidden_notes, 0, wx.EXPAND, 0)
+        self.SetSizer(sizer_4)
+        self.Layout()
+
+class ExtractInfo(wx.Panel):
+    def __init__(self, *args, **kwds):
+        wx.Panel.__init__(self, *args, **kwds)
+        self.label_hidden_notes = wx.StaticText(self, wx.ID_ANY, _(extract_str))
+
+        sizer_4 = wx.BoxSizer(wx.VERTICAL)
+        sizer_4.Add(self.label_hidden_notes, 0, wx.EXPAND, 0)
+        self.SetSizer(sizer_4)
+        self.Layout()
+
+class DuplicateInfo(wx.Panel):
+    def __init__(self, *args, **kwds):
+        wx.Panel.__init__(self, *args, **kwds)
+        self.label_hidden_notes = wx.StaticText(self, wx.ID_ANY, _(duplicate_str))
+
+        sizer_4 = wx.BoxSizer(wx.VERTICAL)
+        sizer_4.Add(self.label_hidden_notes, 0, wx.EXPAND, 0)
+        self.SetSizer(sizer_4)
+        self.Layout()
+
+
+config_config = [("Generic", GenericInfo), ("Ignore Directories and Files", ConfigIgnore),  ("Setting Pathes", ConfigPath)]
+config_dupli = [("Duplication", DuplicateInfo), ("Keep One File", DuplicateKeep),  ("Remove Duplicate Files", DuplicateRemove)]
+config_extract = [("Extraction", ExtractInfo), ("Extract To", ExtractTo),  ("Extract Succeed", ExtractOK)]
+config_rename = [("Rename", ReName)]
+config_same_name = [("Same Name", SameName)]
 
 
 class Config(wx.Treebook):
